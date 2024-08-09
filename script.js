09f5239958e6268149d9d12fc572086c549c4f83
Salomeb22 <salomeberidze76@gmail.com>
2024-08-09 15:39:28 +0400

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1. a set of statements that performs a task or calculates a value
+
+// answer for 2 and 3
+function greet() {
+    console.log('Hello'); // code block
+}
+greet(); // function call
+
+// 4
+function greetName(){
+    let greetName = John;
+    return `Hello, ${greetName}!`;
+}
+
+// 6. Default function parameters allow named parameters to be initialized with default values if no value or undefined is passed.
+// 7. If the variable is in local scope of a function, its lifetime ends when the function ends. If the variable is in global scope, its lifetime ends when the browser tab or window is closed.
+// 8. Return values are just what they sound like — the values that a function returns when it completes.
+// 9. undefined
+
